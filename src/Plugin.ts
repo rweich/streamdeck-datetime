6876b4839acb0fcf850fec357dd56eb716f714e6
Roman Weich <roman.weich@gmail.com>
2021-08-28 18:30:43 +0200

    feat: change style of time and date
    the time is now displayed a little smaller, making the use of the month
    abbreviation and visual differentiation a little easier
    
    allow to set the used font via settings

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,33 +1,26 @@
-import { SettingsType, isSettingsType } from './SettingsType';
+import { SettingsType, defaultSettings, isSettingsType } from './SettingsType';
 
-import SettingsForm from './SettingsForm';
+import Display from './Display';
 import { Streamdeck } from '@rweich/streamdeck-ts';
-import dayjs from 'dayjs';
 
 const UPDATE_INTERVAL_MS = 1000;
 const plugin = new Streamdeck().plugin();
+const display = new Display(plugin);
 const intervalCache: Record<string, NodeJS.Timeout> = {};
 const settingsCache: Record<string, SettingsType> = {};
 
-const onTick = (context: string) => {
-  const settings = settingsCache[context];
-  plugin.setTitle(dayjs().format(settings.format1stLine + '\n' + settings.format2ndLine), context);
-};
+const onTick = (context: string) => display.show(settingsCache[context], context);
 
 plugin.on('willAppear', ({ context }) => {
-  settingsCache[context] = {
-    format1stLine: SettingsForm.default1stLineFormat,
-    format2ndLine: SettingsForm.default2ndLineFormat,
-  };
+  settingsCache[context] = defaultSettings;
   intervalCache[context] = setInterval(() => onTick(context), UPDATE_INTERVAL_MS);
   plugin.getSettings(context);
 });
-plugin.on('willDisappear', ({ context }) => {
-  clearInterval(intervalCache[context]);
-});
+plugin.on('willDisappear', ({ context }) => clearInterval(intervalCache[context]));
 plugin.on('didReceiveSettings', ({ context, settings }) => {
   console.log('got settings', settings);
   if (isSettingsType(settings)) {
+    settingsCache[context].font = settings.font;
     settingsCache[context].format1stLine = settings.format1stLine;
     settingsCache[context].format2ndLine = settings.format2ndLine;
   }
--- src/SettingsType.ts
+++ src/SettingsType.ts
@@ -1,15 +1,24 @@
 import { isSomething } from 'ts-type-guards';
 
 export type SettingsType = {
+  font: string;
   format1stLine: string;
   format2ndLine: string;
 };
 
 export function isSettingsType(payload: unknown): payload is SettingsType {
   return (
-    (payload as SettingsType).hasOwnProperty('format1stLine')
+    (payload as SettingsType).hasOwnProperty('font')
+    && isSomething((payload as SettingsType).font)
+    && (payload as SettingsType).hasOwnProperty('format1stLine')
     && isSomething((payload as SettingsType).format1stLine)
     && (payload as SettingsType).hasOwnProperty('format2ndLine')
     && isSomething((payload as SettingsType).format2ndLine)
   );
 }
+
+export const defaultSettings: SettingsType = {
+  font: 'Verdana',
+  format1stLine: 'HH:mm',
+  format2ndLine: 'D/M',
+};
