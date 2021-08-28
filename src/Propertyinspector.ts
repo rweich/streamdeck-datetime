b94dd68ab3b5c5e8f84056236c0a82371513feed
Roman Weich <roman.weich@gmail.com>
2021-08-28 10:48:25 +0200

    chore: replace pi-form-code with formbuilder
    now we use the formbuilder package instead of creating all the html,
    eventlisteners etc. ourself

--- src/Propertyinspector.ts
+++ src/Propertyinspector.ts
@@ -1,67 +1,19 @@
+import SettingsForm from './SettingsForm';
 import { Streamdeck } from '@rweich/streamdeck-ts';
-import { is } from 'ts-type-guards';
-import { isSettingsType } from './SettingsType';
 
 const pi = new Streamdeck().propertyinspector();
 
-const default1stLineFormat = 'HH:mm';
-const default2ndLineFormat = 'D/M';
-
-const getInput = (name: string): HTMLInputElement | undefined => {
-  const input = document.querySelector("input[name='" + name + "']");
-  if (is(HTMLInputElement)(input)) {
-    return input;
-  }
-  return;
-};
-
-const getInputValue = (name: string): string | undefined => {
-  const input = getInput(name);
-  return input ? input.value : undefined;
-};
-
-const setInputValue = (name: string, value: string): void => {
-  const input = getInput(name);
-  if (input) {
-    input.value = value;
-  }
-};
-
-const onInput = (event: Event): void => {
-  console.log('item changed', event.target, 'event:', event);
-  if (pi.pluginUUID === undefined) {
-    console.error('pi has no uuid! is it registered already?', pi.pluginUUID);
-    return;
-  }
-  if (!is(HTMLInputElement)(event.target)) {
-    return;
-  }
-  pi.setSettings(pi.pluginUUID, {
-    format1stLine: getInputValue('format1stline') || default1stLineFormat,
-    format2ndLine: getInputValue('format2ndline') || default2ndLineFormat,
-  });
-};
-
-pi.on('websocketOpen', ({ uuid }) => {
-  // were there any settings saved?
-  pi.getSettings(uuid);
-
-  // register input event listeners
-  for (const input of Array.from(document.querySelectorAll('.sdpi-item-value'))) {
-    if (is(HTMLInputElement)(input)) {
-      input.addEventListener('input', onInput);
-    }
-  }
-});
+pi.on('websocketOpen', ({ uuid }) => pi.getSettings(uuid));
 
 pi.on('didReceiveSettings', ({ settings }) => {
-  if (isSettingsType(settings)) {
-    setInputValue('format1stline', settings.format1stLine || default1stLineFormat);
-    setInputValue('format2ndline', settings.format2ndLine || default2ndLineFormat);
-  } else {
-    setInputValue('format1stline', default1stLineFormat);
-    setInputValue('format2ndline', default2ndLineFormat);
-  }
+  console.log('got settings', settings);
+  new SettingsForm(pi, settings).onChangeSettings((newSettings) => {
+    if (pi.pluginUUID === undefined) {
+      console.error('pi has no uuid! is it registered already?', pi.pluginUUID);
+      return;
+    }
+    pi.setSettings(pi.pluginUUID, newSettings);
+  });
 });
 
 export default pi;
--- src/SettingsType.ts
+++ src/SettingsType.ts
@@ -1,3 +1,5 @@
+import { isSomething } from 'ts-type-guards';
+
 export type SettingsType = {
   format1stLine: string;
   format2ndLine: string;
@@ -6,6 +8,8 @@ export type SettingsType = {
 export function isSettingsType(payload: unknown): payload is SettingsType {
   return (
     (payload as SettingsType).hasOwnProperty('format1stLine')
+    && isSomething((payload as SettingsType).format1stLine)
     && (payload as SettingsType).hasOwnProperty('format2ndLine')
+    && isSomething((payload as SettingsType).format2ndLine)
   );
 }
