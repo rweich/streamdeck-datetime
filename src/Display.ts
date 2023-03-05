9cccde19311183f3faede988ecee007b08cf4a7b
Roman Weich <roman.weich@gmail.com>
2023-03-05 18:52:03 +0100

    feat: allow changing font-size

--- src/Display.ts
+++ src/Display.ts
@@ -8,6 +8,7 @@ type TextType = { font: string; line1: string; line2: string };
 export default class Display {
   private readonly plugin: Plugin;
   private lastText: Record<string, TextType> = {};
+  private lastSettings: Record<string, Pick<SettingsType, 'fontSize1stLine' | 'fontSize2ndLine'>> = {};
 
   constructor(plugin: Plugin) {
     this.plugin = plugin;
@@ -28,16 +29,16 @@ export default class Display {
     }
 
     const newText = this.getNewText(settings);
-    if (!this.isTextDifferent(pluginContext, newText)) {
+
+    if (this.shouldPreventRedraw(settings, newText, pluginContext)) {
       return;
     }
-    this.lastText[pluginContext] = newText;
 
     context.fillStyle = 'white';
-    context.font = `40px "${newText.font}"`;
+    context.font = `${settings.fontSize1stLine}px "${newText.font}"`;
     context.textAlign = 'center';
     context.fillText(newText.line1, 72, 60, 144);
-    context.font = `28px "${newText.font}"`;
+    context.font = `${settings.fontSize2ndLine}px "${newText.font}"`;
     context.fillText(newText.line2, 72, 110, 144);
     this.plugin.setImage(canvas.toDataURL('image/png'), pluginContext);
   }
@@ -54,6 +55,18 @@ export default class Display {
     };
   }
 
+  private shouldPreventRedraw(settings: SettingsType, newText: TextType, pluginContext: string): boolean {
+    if (!this.isTextDifferent(pluginContext, newText) && !this.didSettingsChange(pluginContext, settings)) {
+      return true;
+    }
+    this.lastText[pluginContext] = newText;
+    this.lastSettings[pluginContext] = {
+      fontSize1stLine: settings.fontSize1stLine,
+      fontSize2ndLine: settings.fontSize2ndLine,
+    };
+    return false;
+  }
+
   private isTextDifferent(pluginContext: string, text: TextType): boolean {
     if (this.lastText[pluginContext] === undefined) {
       this.lastText[pluginContext] = { font: '', line1: '', line2: '' };
@@ -61,5 +74,13 @@ export default class Display {
     const last = this.lastText[pluginContext];
 
     return !(last.line1 === text.line1 && last.line2 === text.line2 && last.font === text.font);
+  }
+
+  private didSettingsChange(pluginContext: string, newSettings: SettingsType): boolean {
+    if (this.lastSettings[pluginContext] === undefined) {
+      return true;
+    }
+    const last = this.lastSettings[pluginContext];
+    return last.fontSize1stLine !== newSettings.fontSize1stLine || last.fontSize2ndLine !== newSettings.fontSize2ndLine;
   }
 }
